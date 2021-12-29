00f8d7c63a1950af8c5318f740c82f88dc1229b1
gesora96 <nahidahmed9933@gmail.com>
2021-12-29 16:03:57 +0600

    day-1 task done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
-// export store =
-
 import { combineReducers, createStore } from 'redux';
 import { bookReducer } from './reducers/bookReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const allState = combineReducers({
   books: bookReducer,
 });
 
-export const store = createStore(allState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(allState, composeWithDevTools());
